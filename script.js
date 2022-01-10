91662de273ff09500d218f747c57f4e64d2b4847
Anton Golub <jukonda23@gmail.com>
2022-01-11 00:01:09 +0200

    feat: finish stage parallax

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ window.onload = function () {
     const parallax = document.querySelector('.parallax');
 
     if(parallax) {
-        const content = document.querySelector('.parallax__container');
         const clouds = document.querySelector('.images-parallax__clouds');
         const mountains = document.querySelector('.images-parallax__mountains');
         const human = document.querySelector('.images-parallax__human');
@@ -21,23 +20,6 @@ window.onload = function () {
 
         //Обьявление переменных
         let positionX = 0, positionY = 0;
-        let coordXprocent = 0, coordYprocent = 0;
-
-        function setMouseParallaxStyle() {
-            const distX = coordXprocent - positionX;
-            const distY = coordYprocent - positionY;
-
-            positionX = positionX + (distX * speed);
-            positionY = positionY + (distY * speed);
-
-            //Передаем стили
-            clouds.style.cssText = `transform: translate(${positionX / forClouds}%,${positionY / forClouds}%);`;
-            mountains.style.cssText = `transform: translate(${positionX / forMountains}%,${positionY / forMountains}%);`;
-            human.style.cssText = `transform: translate(${positionX / forHuman}%,${positionY / forHuman}%);`;
-
-            requestAnimationFrame(setMouseParallaxStyle);
-        }
-        setMouseParallaxStyle();
 
         parallax.addEventListener("mousemove", function (e){
             //Получение высоты и ширины блока
@@ -51,8 +33,21 @@ window.onload = function () {
             //Получаем проценты
             const coordXprocent = coordX / parallaxWidth * 100;
             const coordYprocent = coordY / parallaxHeight * 100;
-            // console.log("y = " + coordYprocent);
-            // console.log("x = " + coordXprocent);
+
+            function setMouseParallaxStyle() {
+                const distX = coordXprocent - positionX;
+                const distY = coordYprocent - positionY;
+
+                positionX = positionX + (distX * speed);
+                positionY = positionY + (distY * speed);
+                //Передаем стили
+                clouds.style.cssText = `transform: translate(${positionX / forClouds}%,${positionY / forClouds}%);`;
+                mountains.style.cssText = `transform: translate(${positionX / forMountains}%,${positionY / forMountains}%);`;
+                human.style.cssText = `transform: translate(${positionX / forHuman}%,${positionY / forHuman}%);`;
+
+                requestAnimationFrame(setMouseParallaxStyle);
+            }
+            setMouseParallaxStyle();
         });
     }
 }
